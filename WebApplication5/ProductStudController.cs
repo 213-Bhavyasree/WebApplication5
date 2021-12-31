47a166e6547866e1a894dfd44b2160c729975915
Admin <kloklokesh846@gmail.com>
2021-12-31 11:08:49 +0530

    Add project files.

---
+++ WebApplication5/ProductStudController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5
+{
+    public class ProductStudController : Controller
+    {
+        public IActionResult IndexStud()
+        {
+            return View();
+        }
+        public IActionResult OneMoreActionStud()
+        {
+            return View();
+        }
+        public IActionResult AnotherActionStud()
+        {
+            return View();
+        }
+            
+    }
+}
---
+++ WebApplication5/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5
+{
+    public class StudentController : Controller
+    {
+        public string Index()
+        {
+            return "This the defalut Action";
+        }
+        public string GetStudentID()
+        {
+            return "This is the StudentID Action";
+        }
+        public string GetStudentName()
+        {
+            return "This is the StudentName Action";
+        }
+    }
+}
